e9ffd58d1564dc07f59bff2c93d2dfeab7bc75f6
Edwin <edwin@gmail.com>
2024-01-06 12:40:07 +1030

    logout button and missing features

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -44,6 +45,7 @@ class MicroPost
     {
         $this->comments = new ArrayCollection();
         $this->likedBy = new ArrayCollection();
+        $this->created = new DateTime();
     }
 
     public function getId(): ?int
